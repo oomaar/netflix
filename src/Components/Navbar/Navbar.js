ab81dc2ba9b155087d64360bff66018c0cf6c771
Omar <omaar_5@hotmail.com>
2021-02-16 00:45:09 +0200

    complete: Navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import "./styles/Navbar.css";
 
 const Navbar = () => {
+    const [show, setShow] = useState(false);
+    
+    const transitionNavbar = () => {
+        window.scrollY > 100 ? setShow(true) : setShow(false);
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", transitionNavbar);
+        return () => window.removeEventListener("scroll", transitionNavbar);
+    }, []);
+
     return (
-        <div>
-            <img src="/images/logo.svg" alt="Netflix Logo" />
-            <img src="/images/users/4.png" alt="User Avatar" />
+        <div className={`navbar ${show && "navbar__black"}`}>
+            <div className="navbar__container">
+                <img className="navbar__logo" src="/images/logo.svg" alt="Netflix Logo" />
+                <img className="navbar__avatar" src="/images/users/4.png" alt="User Avatar" />
+            </div>
         </div>
     )
 };
