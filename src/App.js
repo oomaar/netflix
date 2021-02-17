945b4900ef9e01a5c0c95942f215ede5ad132bfd
Omar <omaar_5@hotmail.com>
2021-02-17 02:31:32 +0200

    dispatch action

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import { login, logout, selectUser } from './features/userSlice';
 import { auth } from './lib/firebase';
 import { HomeScreen, LoginScreen } from './Screens';
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  // const user = null;
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // Logged in
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
       } else {
         // Logged out
+        dispatch(logout);
       }
     });
 
