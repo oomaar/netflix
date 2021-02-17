0f1704d9c582622acc41a689af5007479df69995
Omar <omaar_5@hotmail.com>
2021-02-17 02:15:32 +0200

    firebase code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
+import { auth } from './lib/firebase';
 import { HomeScreen, LoginScreen } from './Screens';
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
 
   return (
     <div className="app">
--- src/Screens/SignupScreen/SignupScreen.js
+++ src/Screens/SignupScreen/SignupScreen.js
@@ -1,25 +1,52 @@
-import React from 'react';
-import { Footer } from '../../Components';
+import { useRef } from 'react';
+import { auth } from '../../lib/firebase';
 import "./styles/SignupScreen.css";
 
 const SignUpScreen = ({ setSignIn }) => {
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+    const register = e => {
+        e.preventDefault();
+
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then(authUser => {
+        // console.log("🚀 ~ file: SignupScreen.js ~ line 17 ~ SignUpScreen ~ authUser", authUser);
+        })
+        .catch(error => alert(error.message));
+    };
+
+    const signIn = e => {
+        e.preventDefault();
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        )
+        .then(authUser => {
+        // console.log("🚀 ~ file: SignupScreen.js ~ line 28 ~ SignUpScreen ~ authUser", authUser);
+        })
+        .catch(error => alert(error.message));
+    }
+
     return (
         <>
             <div className="signup">
                 <img onClick={() => setSignIn(false)} className="signup__closeIcon" src="/images/icons/close-white.png" alt="close" />
                 <form>
                     <h1>Sign In</h1>
-                    <input type="email" placeholder="Email Address" />
-                    <input type="password" placeholder="Password" />
-                    <button type="submit">Sign In</button>
+                    <input ref={emailRef} type="email" placeholder="Email Address" />
+                    <input ref={passwordRef} type="password" placeholder="Password" />
+                    <button onClick={signIn} type="submit">Sign In</button>
 
                     <h4>
                         <span className="signup__gray">New To Netflix? </span>
-                        <span className="signup__link">Sign Up Now.</span>
+                        <span className="signup__link" onClick={register}>Sign Up Now.</span>
                     </h4>
                 </form>
             </div>
-            {/* <Footer /> */}
         </>
     )
 };
