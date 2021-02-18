6f1935d384f1c3201011415834591827fd9091b0
Omar <omaar_5@hotmail.com>
2021-02-18 14:04:10 +0200

    complete: story component

--- src/Components/Story/Story.js
+++ src/Components/Story/Story.js
@@ -4,9 +4,57 @@ import "./styles/Story.css";
 const Story = () => {
     return (
         <div className="story">
-            <h1>The Story Card Container Is Back HAHAHAHAHA</h1>
+            <div className="story__card">
+                <div className="story__feature">
+                    <h1>Enjoy on your TV.</h1>
+                    <h2>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h2>
+                </div>
+                <div className="story__image">
+                    <img className="story__img" src="/images/story/tv.png" alt="story1" />
+                    <div className="story__video">
+                        <video className="story__vid" autoPlay playsInline muted loop>
+                            <source src="/images/story/video-1.m4v" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
+
+            <div className="story__card flip__story">
+                <div className="story__feature flip__feature">
+                    <h1>Download your shows to watch offline.</h1>
+                    <h2>Save your favorites easily and always have something to watch.</h2>
+                </div>
+                <div className="story__image flip__image">
+                    <img className="story__img" src="/images/story/story-2.jpg" alt="story2" />
+
+                    <div className="storyTwo__animation">
+                        <div className="animation__image">
+                            <img className="animation__img" src="/images/story/boxshot.png" alt="BoxShot" />
+                        </div>
+                        <div className="animation__feature">
+                            <div className="animation__featureTitle">Stranger Things</div>
+                            <div className="animation__featureSubTitle">Downloading...</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="story__card">
+                <div className="story__feature">
+                    <h1>Watch everywhere.</h1>
+                    <h2>Stream unlimited movies and TV shows on your phone, tablet, laptop, and TV without paying more.</h2>
+                </div>
+                <div className="story__image">
+                    <img className="story__img" src="/images/story/mac.png" alt="story3" />
+                    <div className="story__video video3">
+                        <video className="story__vid vid3" autoPlay playsInline muted loop>
+                            <source src="/images/story/video-3.m4v" type="video/mp4" />
+                        </video>
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 };
 
 export default Story;
--- src/Screens/LoginScreen/LoginScreen.js
+++ src/Screens/LoginScreen/LoginScreen.js
@@ -7,9 +7,6 @@ import { selectLogin, setLogin } from '../../features/loginSlice';
 import "./styles/LoginScreen.css";
 
 const LoginScreen = () => {
-    // const [signIn, setSignIn] = useState(false);
-    // const user = null;
-
     const login = useSelector(selectLogin);
     const dispatch = useDispatch();
 
@@ -18,30 +15,31 @@ const LoginScreen = () => {
     }
 
     return (
-        <div className="login">
-            <div className="login__bg">
-                <Link to="/">
-                    <img src="/images/logo.svg" alt="Netflix Logo" className="login__logo" />
-                </Link>
-                <button onClick={loginStatus} className="login__button">Sign In</button>
+        <>
+            <div className="login">
+                <div className="login__bg">
+                    <Link to="/">
+                        <img src="/images/logo.svg" alt="Netflix Logo" className="login__logo" />
+                    </Link>
+                    <button onClick={loginStatus} className="login__button">Sign In</button>
 
-                <div className="login__gradient" />
-            </div>
+                    <div className="login__gradient" />
+                </div>
 
-            <div className="login__body">
-                {login ? (
-                    <SignUpScreen  />
-                ) : (
-                        <>
-                            <h1>Unlimited movies, TV shows, and more.</h1>
-                            <h2>Watch anywhere. Cancel anytime.</h2>
-                            
-                            <OptionForm />
-                            <Story />
-                        </>
-                    )}
+                <div className="login__body">
+                    {login ? (
+                        <SignUpScreen />
+                    ) : (
+                            <>
+                                <h1>Unlimited movies, TV shows, and more.</h1>
+                                <h2>Watch anywhere. Cancel anytime.</h2>
+                                <OptionForm />
+                            </>
+                        )}
+                </div>
             </div>
-        </div>
+            <Story />
+        </>
     )
 };
 
